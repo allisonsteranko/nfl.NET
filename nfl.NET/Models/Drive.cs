0e8b68b6f1f93b3488d439ae2e6f521b522daf68
jsteranko <justin.steranko@gmail.com>
2014-08-11 08:42:46 -0400

    missed files

--- nfl.NET/Models/Drive.cs
+++ nfl.NET/Models/Drive.cs
@@ -14,9 +14,18 @@ namespace nfl.NET.Models
         [ForeignKey(typeof(Game))]
         public string gsis_id { get; set; }
         public int FirstDowns { get; set; }
-        public string Text { get; set; }
+        public string result { get; set; }
+        public int PenaltyYards { get; set; }
+        public string TimeOfPossession { get; set; }
+        public int TimeOfPossessionSeconds { get; set; }
+        public int PlayCount { get; set; }
+
         public GameTime StartTime { get; set; }
         public GameTime EndTime { get; set; }
 
+        public FieldPosition StartFieldPos { get; set; }
+        public FieldPosition EndFieldPos { get; set; }
+
+        public List<Play> Plays { get; set; }
     }
 }
--- nfl.NET/Models/LookupTables.cs
+++ nfl.NET/Models/LookupTables.cs
@@ -8,54 +8,54 @@ using System.Threading.Tasks;
 namespace nfl.NET.Models
 {
 
-        public class GameDay
-        {
-            public int GameDayID { get; set; }
-            public string Name { get; set; }
-            public string Abbreviation { get; set; }
-        }
+    public class GameDay
+    {
+        public int GameDayID { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+    }
 
-        public class SeasonPhase
-        {
-            public int SeasonPhaseID { get; set; }
-            public string Name { get; set; }
-            public string Abbreviation { get; set; }
-        }
+    public class SeasonPhase
+    {
+        public int SeasonPhaseID { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+    }
 
-        public class GamePhase
-        {
-            public int GamePhaseID { get; set; }
-            public string Name { get; set; }
-            public string Abbreviation { get; set; }
-        }
+    public class GamePhase
+    {
+        public int GamePhaseID { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+    }
 
-        public class PlayerStatus
-        {
-            public int PlayerStatusID { get; set; }
-            public string Name { get; set; }
-            public string Abbreviation { get; set; }
-        }
+    public class PlayerStatus
+    {
+        public int PlayerStatusID { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+    }
 
-        public class PlayerPosition
-        {
-            public int PositionID { get; set; }
-            public string Name { get; set; }
-            public string Abbreviation { get; set; }
-        }
+    public class PlayerPosition
+    {
+        public int PositionID { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+    }
 
-        public class FieldPosition
-        {
-            public int FieldPositionID { get; set; }
-            public int Position { get; set; }
-            public string Text { get; set; }
-        }
+    public class FieldPosition
+    {
+        public int FieldPositionID { get; set; }
+        public int Position { get; set; }
+        public string Text { get; set; }
+    }
 
-        public class YardageType
-        {
-            public int YardageTypeID { get; set; }
-            public string Name { get; set; }
-            public string Description { get; set; }
-        }
+    public class YardageType
+    {
+        public int YardageTypeID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
 
     public class StatCategory
     {
